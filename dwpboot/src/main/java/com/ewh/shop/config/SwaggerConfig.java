1366f55f729c30b40725efaea54aa859c73164d4
Raj Dahal <rajdahal2010@gmail.com>
2018-10-16 22:54:44 -0400

    Swagger implementation

--- dwpboot/src/main/java/com/ewh/shop/config/SwaggerConfig.java
+++ dwpboot/src/main/java/com/ewh/shop/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.ewh.shop.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +23,13 @@ public class SwaggerConfig {
 	          .select()                                  
 	          .apis(RequestHandlerSelectors.any())              
 	          .paths(PathSelectors.regex("/get.*"))                          
-	          .build();                                           
+	          .build().apiInfo(getApiInfo());                                           
+	    }
+	  
+	  private ApiInfo getApiInfo() {
+		
+		  ApiInfo apiInfo=new ApiInfo("Rest Service ","This service is regardding business ","1.0","Terms of Service: TODO", new Contact("RRR","https://exactlywhathappened.com","raj.jd2@gmail.com"), "Apache 2","https://apache.com", new ArrayList());
+	        return apiInfo;
 	    }
 	
 }
